4176c8d4a335e890f8b522273008bf78972ce946
littlexiaotimpro <a99996655@163.com>
2021-01-07 09:08:06 +0800

    feat:modify the class name

--- cloud-provider-log/src/main/java/com/spring/cloud/provider/log/LogApplication.java
+++ cloud-provider-log/src/main/java/com/spring/cloud/provider/log/LogApplication.java
@@ -1,15 +1,23 @@
 package com.spring.cloud.provider.log;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 日志服务启动类
+ *
+ * @author XiaoSi
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableEurekaClient
-public class Application {
+public class LogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(LogApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 }
