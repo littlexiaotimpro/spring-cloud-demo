0306bca498ceba1ef7943e424b044df194ae31d4
littlexiaotimpro <a99996655@163.com>
2021-01-08 17:12:47 +0800

    feat: Add Spring Security

---
+++ cloud-eureka-server/src/main/java/com/spring/cloud/eureka/server/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.eureka.server.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Web 安全认证配置
+ *
+ * @author XiaoSi
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
