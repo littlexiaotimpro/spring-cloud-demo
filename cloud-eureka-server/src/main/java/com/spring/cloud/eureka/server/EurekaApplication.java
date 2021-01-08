76b365322fd57f7ee283ac05d007cce35cc70ce6
littlexiaotimpro <a99996655@163.com>
2021-01-08 15:11:12 +0800

    feat:cloud cluster config

--- cloud-eureka-server/src/main/java/com/spring/cloud/eureka/server/EurekaApplication.java
+++ cloud-eureka-server/src/main/java/com/spring/cloud/eureka/server/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.eureka.server;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(EurekaApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 
 }
