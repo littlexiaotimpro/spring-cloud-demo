69683ef8aec860b86e2729a3bac73ea5076ff4e4
littlexiaotimpro <a99996655@163.com>
2021-01-19 12:07:19 +0800

    feat: add spring cloud ribbon dependency

--- spring-cloud-demo-client/src/main/java/com/spring/cloud/eureka/client/config/AutoConfiguration.java
+++ spring-cloud-demo-client/src/main/java/com/spring/cloud/eureka/client/config/AutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.eureka.client.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,11 @@ import org.springframework.web.client.RestTemplate;
 public class AutoConfiguration {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+
 
 }
