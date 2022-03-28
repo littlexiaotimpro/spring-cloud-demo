d28d1dba65b44f4fca450a988880b49931a019fe
littlexiaotimpro <a99996655@163.com>
2022-03-28 17:17:06 +0800

    feat: create module of gateway

---
+++ a-cloud-base-gateway/src/main/java/com/spring/cloud/base/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.base.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 网关服务启动类
+ *
+ * @since 2022/3/28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
